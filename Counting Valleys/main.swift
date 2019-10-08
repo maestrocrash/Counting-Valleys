757f4413864fde8da8f9ae7a5c0d96417e9a8f50
maestrocrash <maestrocrash@gmail.com>
2019-10-08 16:04:22 +0300

    Initial Commit

---
+++ Counting Valleys/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Counting Valleys
+//
+//  Created by Shakhvorostov on 08.10.2019.
+//  Copyright © 2019 Shakhvorostov. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
