a05c83966b25d93d4b1bfcfab6789c4d73c6e1c8
maestrocrash <maestrocrash@gmail.com>
2019-10-09 11:10:05 +0300

    1 variante no work

--- Counting Valleys/main.swift
+++ Counting Valleys/main.swift
@@ -8,5 +8,38 @@
 
 import Foundation
 
-print("Hello, World!")
+func countingValleys(n: Int, s: String) -> Int {
+    var i: Int = 0
+    var str: String = s
+    var index: Int = 0
+    var m: Int = 0
+    
+    func change(i: Int) -> Int{
+        if i == 0 {
+            index += 1
+        }
+        return index
+    }
+    
+    while i != str.count {
+        if str.first == "U" {
+            i += 1
+            m += 1
+            str.removeFirst()
+        } else {
+            i -= 1
+            m -= 1
+            str.removeFirst()
+        }
+        if abs(m) >= 2 {
+            i = 0
+            change(i: i)
+           
+        }
+        
+    }
+    return index
+}
+
+print(countingValleys(n: 8, s: "UDDDUDUU"))
 
